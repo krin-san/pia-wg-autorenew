539522dda2117302dff06bd3f980198d6f4052e9
Alexander Chapliuk <krin.chap@gmail.com>
2022-12-30 14:48:47 +0100

    Refactor

--- generate-config-auto.py
+++ generate-config-auto.py
@@ -1,52 +1,51 @@
-import time
-import sys
-import logging
-from datetime import datetime
-import os
-
 import json
+import logging
+import os
+import time
+from datetime import datetime
 
 from piawg import piawg
 
+
+REQUIRED_KEYS = {
+    "USERNAME" : str,
+    "PASSWORD" : str,
+    "UPDATE_INTERVAL" : int,
+    "REGION" : str
+}
+WG_CONFIG_FILE='/app/wg0.conf'
+
+
 class PiaWGDaemon:
     def __init__(self):
-
-
-        self.req_key = {
-            "USERNAME" : str,
-            "PASSWORD" : str,
-            "UPDATE_INTERVAL" : int,
-            "REGION" : str
-        }
+        logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
 
         self.config = {}
 
-        logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
-        for key in self.req_key:
-                current_param = os.environ.get(key)
+        for key in REQUIRED_KEYS:
+            current_param = os.environ.get(key)
 
-                if current_param is None:
-                    error_msg = "{} key was not found in enviorment".format(key)
+            if current_param is None:
+                error_msg = "{} key was not found in the environment".format(key)
+
+                logging.error(error_msg)
+                raise KeyError(error_msg)
+
+            if not isinstance(current_param, REQUIRED_KEYS[key]):
+                found_type = type(current_param)
+                try:
+                    current_param = REQUIRED_KEYS[key](current_param)
+                except:
+                    error_msg = "{} key was of a wrong type. Found: {} Expected: {}".format(
+                        key,
+                        found_type,
+                        REQUIRED_KEYS[key]
+                    )
 
                     logging.error(error_msg)
-                    raise KeyError("Required Key {} Was not found in json")
+                    raise KeyError(error_msg)
 
-                if not isinstance(current_param, self.req_key[key]):
-                    found_type = type(current_param)
-                    try:
-                        current_param = self.req_key[key](current_param)
-                    except:
-                        error_msg = "{} key was of wrong type. Found : {} Expected: {} ".format(
-                            key,
-                            found_type,
-                            self.req_key[key]
-                        )
-
-                        logging.error(error_msg)
-                        raise KeyError(error_msg)
-
-
-                self.config[key] = current_param
+            self.config[key] = current_param
 
         pia = piawg()
 
@@ -58,96 +57,66 @@ class PiaWGDaemon:
             logging.error(error_msg)
             raise KeyError(error_msg)
 
-
-        '''
-        with open(CONFIG_FILE, "r") as f:
-            self.config = json.load(f)
-            for key in self.req_key:
-                if key not in self.config:
-                    error_msg = "{} key was not found in json".format(key)
-                    logging.error(error_msg)
-                    raise KeyError("Required Key {} Was not found in json")
-
-                if not isinstance(self.config[key], self.req_key[key]):
-                    error_msg = "{} key was of wrong type. Found : {} Expected: {} ".format(
-                        key,
-                        type(self.config[key]),
-                        self.req_key[key]
-                    )
-
-                    logging.error(error_msg)
-                    raise KeyError(error_msg)
-
-                pia = piawg()
-
-                if self.config["region"] not in pia.server_list.keys():
-                    error_msg = "{} Region is not a valid one ".format(
-                        self.config["region"]
-                    )
-
-                    logging.error(error_msg)
-                    raise KeyError(error_msg)
-        '''
-
-        self.timout_period = 5
-
+        self.loop_delay = 5
+        self.last_config_update = 0
 
     def event_loop(self):
-        last_update = 0
         try:
             while True:
-                print("files", os.listdir("."))
-                if time.monotonic() - last_update >= self.config["UPDATE_INTERVAL"]:
-                    logging.info("Updating Token")
+                if time.monotonic() - self.last_config_update < self.config["UPDATE_INTERVAL"]:
+                    time.sleep(self.loop_delay)
+                    continue
 
-                    pia = piawg()
-                    pia.generate_keys()
-                    pia.set_region(self.config["REGION"])
-
-                    if pia.get_token(self.config["USERNAME"], self.config["PASSWORD"]):
-                        successful_login = True
-                        logging.info("Login Succesful")
-                    else:
-                        logging.error("Error Logging In")
-                        successful_login = False
-
-                    last_update = time.monotonic()
-
-                    if not successful_login:
-                        continue
-
-                    status, response = pia.addkey()
-
-                    if status:
-                        logging.info("Added key to server!")
-                    else:
-                        logging.error("Error adding key to server")
-                        logging.error(response)
-                        continue
-
-                    self.write_file(pia)
-
-                else:
-                    time.sleep(self.timout_period)
+                pia = piawg()
+                pia.set_region(self.config["REGION"])
+                self.update_wireguard_config(pia)
 
         except KeyboardInterrupt:
             logging.info("Event Loop Interrupted, exiting")
             exit()
 
+    def update_wireguard_config(self, pia):
+        logging.info("Updating Token")
+
+        pia.generate_keys()
+
+        if pia.get_token(self.config["USERNAME"], self.config["PASSWORD"]):
+            successful_login = True
+            logging.info("Login Successful")
+        else:
+            logging.error("Error Logging In")
+            successful_login = False
+
+        self.last_config_update = time.monotonic()
+
+        if not successful_login:
+            return
+
+        status, response = pia.add_key()
+
+        if status:
+            logging.info("Added key to server!")
+        else:
+            logging.error("Error adding key to server")
+            logging.error(response)
+            return
+
+        self.write_file(pia)
 
     def write_file(self, pia):
-        logging.info("Writing Config")
-        timestamp = int(datetime.now().timestamp())
-        with open("/app/wg0.conf", 'w+') as file:
+        logging.info("Writing Wireguard config")
+
+        with open(WG_CONFIG_FILE, 'w+') as file:
             file.write('[Interface]\n')
             file.write('Address = {}\n'.format(pia.connection['peer_ip']))
-            file.write('PrivateKey = {}\n'.format(pia.privatekey))
+            file.write('PrivateKey = {}\n'.format(pia.private_key))
             file.write('DNS = {},{}\n\n'.format(pia.connection['dns_servers'][0], pia.connection['dns_servers'][1]))
             file.write('[Peer]\n')
             file.write('PublicKey = {}\n'.format(pia.connection['server_key']))
             file.write('Endpoint = {}:1337\n'.format(pia.connection['server_ip']))
             file.write('AllowedIPs = 0.0.0.0/0\n')
             file.write('PersistentKeepalive = 25\n')
+
 
 if __name__ == "__main__":
     PiaWGDaemon().event_loop()
--- piawg.py
+++ piawg.py
@@ -1,9 +1,10 @@
-import requests
 import json
-from requests_toolbelt.adapters import host_header_ssl
-import urllib3
+import requests
 import subprocess
 import urllib.parse
+import urllib3
+from requests_toolbelt.adapters import host_header_ssl
+
 
 # PIA uses the CN attribute for certificates they issue themselves.
 # This will be deprecated by urllib3 at some point in the future, and generates a warning (that we ignore).
@@ -13,12 +14,35 @@ urllib3.disable_warnings(urllib3.exceptions.SubjectAltNameWarning)
 class piawg:
     def __init__(self):
         self.server_list = {}
-        self.get_server_list()
         self.region = None
         self.token = None
-        self.publickey = None
-        self.privatekey = None
+        self.public_key = None
+        self.private_key = None
         self.connection = None
+
+        self.get_server_list()
+
+
+    def __is_valid_response(self, response):
+        return response.status_code == 200 and response.json()['status'] == 'OK'
+
+    # Common name and IP address for metadata endpoint in region
+    def meta_server(self):
+        server = self.server_list[self.region]['servers']['meta'][0]
+        return server['cn'], server['ip']
+
+    # Common name and IP address for wireguard endpoint in region
+    def wireguard_server(self):
+        server = self.server_list[self.region]['servers']['wg'][0]
+        return server['cn'], server['ip']
+
+    def __verified_session(self):
+        # Some tricks to verify PIA certificate, even though we're sending requests to an IP and not a proper domain
+        # https://toolbelt.readthedocs.io/en/latest/adapters.html#requests_toolbelt.adapters.host_header_ssl.HostHeaderSSLAdapter
+        session = requests.Session()
+        session.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
+        session.verify = 'ca.rsa.4096.crt'
+        return session
 
     def get_server_list(self):
         r = requests.get('https://serverlist.piaservers.net/vpninfo/servers/v4')
@@ -31,42 +55,44 @@ class piawg:
         self.region = region_id
 
     def get_token(self, username, password):
-        # Get common name and IP address for metadata endpoint in region
-        meta_cn = self.server_list[self.region]['servers']['meta'][0]['cn']
-        meta_ip = self.server_list[self.region]['servers']['meta'][0]['ip']
+        cn, ip = self.meta_server()
+        r = self.__verified_session().get(
+            "https://{}/authv3/generateToken".format(ip),
+            headers={"Host": cn},
+            auth=(username, password)
+        )
 
-        # Some tricks to verify PIA certificate, even though we're sending requests to an IP and not a proper domain
-        # https://toolbelt.readthedocs.io/en/latest/adapters.html#requests_toolbelt.adapters.host_header_ssl.HostHeaderSSLAdapter
-        s = requests.Session()
-        s.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
-        s.verify = 'ca.rsa.4096.crt'
-
-        r = s.get("https://{}/authv3/generateToken".format(meta_ip), headers={"Host": meta_cn},
-                  auth=(username, password))
-        data = r.json()
-        if r.status_code == 200 and data['status'] == 'OK':
-            self.token = data['token']
+        if self.__is_valid_response(r):
+            self.token = r.json()['token']
             return True
         else:
             return False
 
     def generate_keys(self):
-        self.privatekey = subprocess.run(['wg', 'genkey'], stdout=subprocess.PIPE, encoding="utf-8").stdout.strip()
-        self.publickey = subprocess.run(['wg', 'pubkey'], input=self.privatekey, stdout=subprocess.PIPE,
-                                        encoding="utf-8").stdout.strip()
+        self.private_key = subprocess.run(
+            ['wg', 'genkey'],
+            stdout=subprocess.PIPE,
+            encoding="utf-8"
+        ).stdout.strip()
+        self.public_key = subprocess.run(
+            ['wg', 'pubkey'],
+            input=self.private_key,
+            stdout=subprocess.PIPE,
+            encoding="utf-8"
+        ).stdout.strip()
 
-    def addkey(self):
-        # Get common name and IP address for wireguard endpoint in region
-        cn = self.server_list[self.region]['servers']['wg'][0]['cn']
-        ip = self.server_list[self.region]['servers']['wg'][0]['ip']
+    def add_key(self):
+        cn, ip = self.wireguard_server()
+        r = self.__verified_session().get(
+            "https://{}:1337/addKey?pt={}&pubkey={}".format(
+                ip,
+                urllib.parse.quote(self.token),
+                urllib.parse.quote(self.public_key)
+            ),
+            headers={"Host": cn}
+        )
 
-        s = requests.Session()
-        s.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
-        s.verify = 'ca.rsa.4096.crt'
-
-        r = s.get("https://{}:1337/addKey?pt={}&pubkey={}".format(ip, urllib.parse.quote(self.token),
-                                                                  urllib.parse.quote(self.publickey)), headers={"Host": cn})
-        if r.status_code == 200 and r.json()['status'] == 'OK':
+        if self.__is_valid_response(r):
             self.connection = r.json()
             return True, r.content
         else:
